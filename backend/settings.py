bd13097eb685ff6e1f3b76e5ea755c9af1078a63
ceomarin <cmarin.ing@gmail.com>
2022-11-09 11:30:32 -0300

    add host allowed

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['https://bsale-test-u6r8.onrender.com/api/producto/']
+ALLOWED_HOSTS = ['http://127.0.0.1:5000']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
